4bc16f01bf8247c21776165a139a51b7136697b3
Adam Groszer <agroszer@gmail.com>
2013-05-08 12:58:17 +0200

    windows support

--- gp/recipe/phantomjs/__init__.py
+++ gp/recipe/phantomjs/__init__.py
@@ -16,10 +16,17 @@ class Recipe(object):
 
     def get_binaries(self):
         binaries = glob.glob(os.path.join(self.install_dir, '*', 'bin', '*js'))
-        binaries = dict([(os.path.basename(p), p) for p in binaries])
-        if 'bootstrap.js' in binaries:
-            del binaries['bootstrap.js']
-        return binaries
+        if sys.platform.startswith('win'):
+            binaries.extend(glob.glob(os.path.join(self.install_dir, '*', '*.exe')))
+        rv = {}
+        for p in binaries:
+            bname = os.path.basename(p)
+            if bname.lower().endswith('.js'):
+                continue
+            if bname.lower().endswith('.exe'):
+                bname = bname[:-4]
+            rv[bname] = p
+        return rv
 
     def download(self, url):
         from hexagonit.recipe.download import Recipe as Download
@@ -45,6 +52,11 @@ class Recipe(object):
                     url = (
                         'https://phantomjs.googlecode.com/'
                         'files/phantomjs-%s-macosx.zip'
+                    ) % version
+                elif sys.platform.startswith('win'):
+                    url = (
+                        'https://phantomjs.googlecode.com/'
+                        'files/phantomjs-%s-windows.zip'
                     ) % version
                 else:
                     raise RuntimeError('Please specify a phantomjs-url')
